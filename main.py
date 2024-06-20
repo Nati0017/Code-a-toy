10b88923ea0af563eb5ee05addbb4ea5b086d1c5
Natasha Pérez <165829944+Nati0017@users.noreply.github.com>
2024-06-20 16:40:30 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+shopping_list= []
+
+print(" Buenas tardes, bienvenido a tu carrito de compras!! ")
+input("")
+
+item_1 = input("¿Que deseas comprar? ")
+
+item_2 = input("¿Deseas agregar algo más? ")
+
+item_3 = input(" ¿Algo más? ")
+
+shopping_list= "Tienes: "+ item_1 +","  + item_2 +","  + item_3 + ""
+
+print(shopping_list)
+
+print(" Espero tengas buen día")
+print(" Por favor procede a pagar tu carrito!! Muchas gracias")
